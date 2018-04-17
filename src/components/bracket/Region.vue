84cb095c80503c3d6008880d7fe2430a3830bc33
Aaron Elizondo <acelizondo11@gmail.com>
2018-04-17 12:57:48 -0500

    Adding some styling to the songcell grid

--- src/components/bracket/Region.vue
+++ src/components/bracket/Region.vue
@@ -3,7 +3,7 @@
         <div class="cell rdColumn" v-for="(rdData, rdName) in region.rds">
             <div class="rdHeader">{{ rdName }}</div>
             <div class="songCell" :class="rdName" v-for="song in rdData">
-                {{ song.songName }}
+                <span>{{ song.songName }}</span>
             </div>
         </div>
     </div>
@@ -11,13 +11,34 @@
 
 <script>
     export default {
-        props: ['region', 'orientation']
+        data: () => {
+            return {
+                regionRds: ''
+            };
+        },
+        props: ['region', 'orientation'],
+        methods: {
+            reverseRegion(){
+                var order = [];
+                for(var key in this.region.rds){
+                    order.push(key);
+                }
+                return order;
+            }
+        },
+        created(){
+            if(this.orientation === 'right'){
+                this.regionRds = this.reverseRegion();
+            } else {
+                this.regionRds = this.region.rds;
+            }
+        }
     }
 </script>
 
 <style scoped>
 .rdCell{
-    font-size: 13px;
+    font-size: 0.74rem;
     text-align: center;
 }
 
@@ -26,11 +47,20 @@
 }
 
 .rdHeader{
-    height: 4.88vh;
+    height: 3vh;
+    text-decoration: underline;
 }
 
 .songCell{
-    height: 4.88vh;
+    height: 4.5vh;
+    margin-top: 0.45vh;
+    box-shadow: 0.1rem 0.1rem 0.1rem 0.1rem #ddd;
+    /* padding-top: 0.25vh; */
+}
+
+.songCell>span{
+    display:inline-block;
+    vertical-align: -webkit-baseline-middle;
 }
 
 </style>
