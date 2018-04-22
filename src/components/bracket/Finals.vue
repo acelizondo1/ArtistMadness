5d20a9fff7c8eaac77b57a18e31c2770a680f391
Aaron Elizondo <acelizondo11@gmail.com>
2018-04-21 22:34:46 -0500

    Started to add the foundation grid for the finals component

--- src/components/bracket/Finals.vue
+++ src/components/bracket/Finals.vue
@@ -1,6 +1,32 @@
 <template>
-  <div class="cell small-10 float-center finals">
-      {{region}}
+  <div class="grid-x finals">
+      <div class="cell small-1"></div>
+      <div class="cell small-3 final-half final-half-left">
+          <div class="grid-y">
+              <div class="cell small-2 small-offset-2 final-four">
+
+              </div>
+              <div class="cell small-2 small-offset-4 final-four">
+
+              </div>
+          </div>
+      </div>
+      <div class="cell small-4 championship">
+          <div class="grid-y">
+
+          </div>
+      </div>
+      <div class="cell small-3 final-half final-half-right">
+          <div class="grid-y">
+              <div class="cell small-2 small-offset-2 final-four">
+
+              </div>
+              <div class="cell small-2 small-offset-4 final-four">
+
+              </div>
+          </div>
+      </div>
+      <div class="cell small-1"></div>
   </div>
 </template>
 
@@ -12,7 +38,12 @@
 
 <style scoped>
     .finals{
-        height: 70vh;
-        margin-top: 6vh;
+        height: 83vh;
+        width: 100%;
+    }
+
+    .final-four{
+        /* margin-top: 16.666667%;
+        margin-bottom: 16.666667%; */
     }
 </style>
